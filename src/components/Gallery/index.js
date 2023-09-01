5ab9ed1d7ef20b979cb9ee207f606a955d73493f
Vlad-Beresnev <beresnev.explorer@gmai.com>
2023-09-01 11:16:04 +0300

    added video gallery, basis parallax library

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,34 @@
+import "./index.scss"
+import React from "react"
+import ReactPlayer from "react-player"
+
+
+
+const Gallery = () => {
+
+
+    return (
+        <div className="gallery">
+            <div className="video">
+                <ReactPlayer className="videor" url="https://youtu.be/Cxfu_mc2SnI?si=J2-x-Cie_BDqdN5F" youtube width="100%" />
+            </div>
+            <div className="video">
+                <ReactPlayer className="videor" url="https://youtu.be/6YuwooKgmDE?si=7i3MsjQUj2mHpBzb" width="100%"/>
+            </div>
+            <div className="video">
+                <ReactPlayer className="videor" url="https://youtu.be/PI5vSs3NZhA?si=gs5fSa8yoUWUS5Jv" width="100%"/>
+            </div>
+            <div className="video">
+                <ReactPlayer className="videor" url="https://youtu.be/Eju8bb5a8lo?si=xQGExJyacD9vJRSy" width="100%"/>
+            </div>
+            <div className="video">
+                <ReactPlayer className="videor" url="https://youtu.be/PmSjOF8cWPY?si=Bf6h3NiPHMg2zN0c" width="100%"/>
+            </div>
+            <div className="video">
+                <ReactPlayer className="videor" url="https://youtu.be/Nrp8nMBNY4o?si=jh6psLWNeylUrJJA" width="100%"/>
+            </div>
+        </div>
+    )
+}
+
+export default Gallery
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,36 +1,66 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import Loader from 'react-loaders';
+import GandiPhoto from "../../assets/images/gandi-2.png"
+import Gallery from "../../components/Gallery"
+import { Parallax, ParallaxLayer } from "@react-spring/parallax"
 
 
 const Home = () => {
         const [letterClass, setLetterClass] = useState('text-animate');
-        const nameArray = ['S', 'u', 'g', 'a', 'n', 'd', 'i', '', 'M', 'o', 'e', 'n', 't', 'h', 'e'];
+        const nameArray = ['G', 'a', 'n', 'd', 'i', '', 'M', 'o', 'e', 'n', 't', 'h', 'e'];
         const jobArray = [ 's', 'a', 'l', 'e', 'r'];
+        const calculatePages = () => {
+            const screenWidth = window.innerWidth;
+            if (screenWidth < 1157 & screenWidth > 799 ) {
+                return 3;
+            } if (screenWidth < 777) {
+                return 4;
+            } else {
+                return 2;
+            }
+        }
     
-       
+        useEffect(() => {
+            const handleResize = () => {
+                setPages(calculatePages());
+            };
+    
+            window.addEventListener('resize', handleResize);
+            return () => {
+                window.removeEventListener('resize', handleResize);
+            }
+        }, []);
 
+    const [pages, setPages] = useState(calculatePages());
+        
     return (
         <>
-        
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                idx={15}/>
-                <br />
-                
-                </h1>
-                <h2>All the time God is very good!</h2>
-                <Link to="/contact" className='flat-buttom'>CONTACT ME</Link>
-            </div>
-            
-        </div>
-        <Loader type="line-scale-pulse-out" />
+        <Parallax pages={pages}>
+            <ParallaxLayer>
+                <div className="container home-page">
+                    <div className='main-photo'>
+                        <img src={GandiPhoto} alt="gandi-photo" />
+                    </div>
+                    <div className="text-zone">
+                        <h1>
+                        <AnimatedLetters letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15}/>
+                        
+                        </h1>
+                        {/*<h2>All the time God is very good!</h2>*/}
+                        <Link to="/contact" className='flat-buttom'>CONTACT ME</Link>
+                    </div>
+                </div>
+                <Loader type="line-scale-pulse-out" />
+            </ParallaxLayer>
+            <ParallaxLayer offset={1}>
+                <Gallery />
+            </ParallaxLayer>
+        </Parallax>
         </>
     );
 
