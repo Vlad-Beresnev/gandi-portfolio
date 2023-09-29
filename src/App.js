687dfd0abc977b27ce242adb2223cbefef9ca837
Vlad-Beresnev <beresnev.explorer@gmai.com>
2023-09-29 11:21:26 +0300

    f

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="../../components/Work" element={<Work />} />
+        <Route path="work" element={<Work />} />
         <Route path="tour" element={<Tour />} />
       </Route>
     </Routes>
